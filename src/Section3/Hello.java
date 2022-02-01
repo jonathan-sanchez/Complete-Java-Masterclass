37b6142fb39d390e75c9401a3d4c03445ff4d26a
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-01 16:30:14 -0600

    added: section 3 package, along with hello class

--- src/Section3/Hello.java
+++ src/Section3/Hello.java
@@ -1,8 +1,25 @@
 package Section3;
 
-public class Hello {
+public class Hello {    //the public java keyword is an access modifier.
+						//the class keyword is used to define a class with the name following the keyword - Hello in this case.
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {    //static is needed for java to find our method to run the code we are going to add.
+												//A code block is used to define a block of code. It is mandatory to have one in a method declaration.
+		System.out.println("Hello, Jon");       //a statement is a complete command to be executed and can include one or more expressions.
 
+		int myFirstNumber = (10 + 5) + (2 * 10);
+//		System.out.println(myFirstNumber);
+
+		int mySecondNumber = 12;
+//		System.out.println(mySecondNumber);
+
+		int myThirdNumber = myFirstNumber * 2;
+//		System.out.println(myThirdNumber);
+
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+//		System.out.println(myTotal);
+
+		int myLastOne = 1000 - myTotal;
+		System.out.println(myLastOne);
 	}
 }
