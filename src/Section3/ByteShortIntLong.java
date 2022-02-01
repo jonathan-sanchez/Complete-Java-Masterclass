87ca3a4c92e62e09a9f6e87c6dcac3391b8473ff
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-01 17:52:00 -0600

    completed: ByteShortIntLong.java

--- src/Section3/ByteShortIntLong.java
+++ src/Section3/ByteShortIntLong.java
@@ -7,7 +7,7 @@ public class ByteShortIntLong {
 		int myValue = 10000;
 
 		//Int (much larger range, occupies 32 bits - has a width of 32)
-		int myMinIntValue = Integer.MIN_VALUE;
+		int myMinIntValue = Integer.MIN_VALUE;      //java uses wrapper classes for all eight primitive types. In case of an int we can use Integer
 		int myMaxIntValue = Integer.MAX_VALUE;
 		System.out.println("Integer Minimum Value = " + myMinIntValue);
 		System.out.println("Integer Maximum Value = " + myMaxIntValue);
@@ -24,6 +24,33 @@ public class ByteShortIntLong {
 		System.out.println();
 
 		//Byte (occupies 8 bits - has a width of 8)
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("Byte Minimum Value = " + myMinByteValue);
+		System.out.println("Byte Maximum Value = " + myMaxByteValue);
 
+		System.out.println();
+
+		//Short (can store a large range of numbers and occupies 16 bits - width of 16)
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("Short Minimum Value = " + myMinShortValue);
+		System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+		System.out.println();
+
+		//Long
+		long myLongValue = 100L;
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		System.out.println("Long Minimum Value = " + myMinLongValue);
+		System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+		System.out.println();
+
+		long bigLongLiteralValue = 2147483647234L;
+		System.out.println(bigLongLiteralValue);
+
+		short bigSortLiteralValue = 32767;
 	}
 }
