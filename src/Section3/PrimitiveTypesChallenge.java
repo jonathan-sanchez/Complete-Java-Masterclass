9f7494bbbafc2a4b33d1e4a152fdec5a86f0dd83
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-02 17:25:00 -0600

    feat: completed PrimitiveTypesChallenge.java

--- src/Section3/PrimitiveTypesChallenge.java
+++ src/Section3/PrimitiveTypesChallenge.java
@@ -7,8 +7,11 @@ public class PrimitiveTypesChallenge {
 		byte byteValue = 10;
 		short shortValue = 20;
 		int intValue = 50;
-
+		//Without casting
 		long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
 		System.out.println(longTotal);
+		//With casting
+		short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+		System.out.println(shortTotal);
 	}
 }
