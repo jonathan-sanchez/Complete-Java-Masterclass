580194c450717a24eeffc528b63bfa55c5074c94
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-02 17:22:14 -0600

    feat: completed longTotal variable without casting

--- src/Section3/PrimitiveTypesChallenge.java
+++ src/Section3/PrimitiveTypesChallenge.java
@@ -4,5 +4,11 @@ public class PrimitiveTypesChallenge {
 
 	public static void main(String[] args) {
 
+		byte byteValue = 10;
+		short shortValue = 20;
+		int intValue = 50;
+
+		long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+		System.out.println(longTotal);
 	}
 }
