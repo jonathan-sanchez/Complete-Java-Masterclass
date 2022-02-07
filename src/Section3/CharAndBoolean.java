0e0a709f7d941a1b1841ef34b5910b78bb642d2c
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-07 15:06:21 -0600

    added/completed: CharAndBoolean.java

---
+++ src/Section3/CharAndBoolean.java
@@ -0,0 +1,21 @@
+package Section3;
+
+public class CharAndBoolean {
+
+	public static void main(String[] args) {
+
+		//Char and Boolean
+		char myChar = 'D'; //Can only store a single character in char type
+		char myUnicodeChar = '\u0044'; //*Unicode*
+		System.out.println(myChar);
+		System.out.println(myUnicodeChar);
+		char myCopyrightChar = '\u00A9'; //*Unicode*
+		System.out.println(myCopyrightChar);
+
+		// Boolean
+		boolean myTrueBooleanValue = true;
+		boolean myFalseBooleanValue = false;
+
+		boolean isCustomerOverTwentyOne = true;
+	}
+}
