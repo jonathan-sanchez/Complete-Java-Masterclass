3147da3d23e98f9c2cf0189f5fcc93f192d627e9
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-08 15:29:27 -0600

    feat: added logical operators to Operators.java

--- src/Section3/Operators.java
+++ src/Section3/Operators.java
@@ -45,5 +45,69 @@ public class Operators {
 		//result = result - 2
 		result -= 2;    //10 - 2 = 8
 		System.out.println("10 - 2 = " + result);
+
+		// if-then Statement
+
+		boolean isAlien = false;
+		if (isAlien == false) {
+			System.out.println("It is not an Alien!");
+			System.out.println("And i am scared of aliens");
+		}
+
+		//Logical AND Operator (&&)
+
+		int topScore = 80;
+		if (topScore < 100) {
+			System.out.println("You got the high score!");
+		}
+
+		int secondTopScore = 81;
+		if ((topScore > secondTopScore) && (topScore < 100)) {
+			System.out.println("Greater than second top score and less than 100");
+		}
+
+		//logical OR operator (||)
+
+		if ((topScore > 90) || (secondTopScore <= 90)) {
+			System.out.println("Either or both of the conditions are true");
+		}
+
+		//Assignment Operator VS Equals Operator
+
+		int newValue = 50;
+		if (newValue == 50) {
+			System.out.println("This is true");
+		}
+
+		boolean isCar = false;
+		if (isCar) {
+			System.out.println("This is not supposed to happen");
+		}
+
+		//Ternary operator
+		isCar = true;
+		boolean wasCar = isCar ? true : false;
+		if (wasCar) {
+			System.out.println("wasCar is true");
+		}
+
+		//Operator Challenge
+
+		double firstVariable = 20.00d;
+
+		double secondVariable = 80.00d;
+
+		double total = ((firstVariable + secondVariable) * 100.00d);
+		System.out.println("My total is " + total);
+
+		double theRemainder = total % 40.00d;
+		System.out.println("The remainder = " + theRemainder);
+
+		boolean isNoRemainder = (theRemainder == 0) ? true : false;
+		System.out.println("isNoRemainder = " + isNoRemainder);
+
+		if (!isNoRemainder) {
+			System.out.println("Got some remainder");
+		}
 	}
 }
