0c0c665c397a6e36e5d14664e0a3f708f21374b4
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-08 16:07:20 -0600

    completed: StatementsWhiteSpaceAndIndenting.java

--- src/Section4/StatementsWhiteSpaceAndIndenting.java
+++ src/Section4/StatementsWhiteSpaceAndIndenting.java
@@ -4,7 +4,19 @@ public class StatementsWhiteSpaceAndIndenting {
 
 	public static void main(String[] args) {
 
+		int myVariable = 50;
+		myVariable++;
+		if (myVariable == 50) {
+			System.out.println("This is a test");
+		}
 
+		System.out.println("This is" +
+				" another" +
+				" still more.");
+
+		int anotherVariable = 50;
+		myVariable--;
+		System.out.println("This is another one");
 
 	}
 }
