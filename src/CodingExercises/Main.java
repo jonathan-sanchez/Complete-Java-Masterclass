f9521c2fd2e6a20c317c63c2451d2c0da2913c87
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-09 15:20:53 -0600

    completed SpeedConverter.java

--- src/CodingExercises/Main.java
+++ src/CodingExercises/Main.java
@@ -3,6 +3,11 @@ package CodingExercises;
 public class Main {
 
 	public static void main(String[] args) {
+		//Exercise 1: Speed Converter
+//		SpeedConverter.printConversion(75.114);
+
+		//Exercise 2: MegaBytes Converter
+
 
 	}
 }
