30b671f31b52f707f388f0901bcb77c6de99999c
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-16 10:20:32 -0600

    completed: DecimalComparator.java

--- src/CodingExercises/DecimalComparator.java
+++ src/CodingExercises/DecimalComparator.java
@@ -2,7 +2,11 @@ package CodingExercises;
 
 public class DecimalComparator {
 
-	public static boolean areEqualByThreeDecimalPlaces() {
+	public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
 
+		int xInt = (int) (x * 1000);
+		int yInt = (int) (y * 1000);
+
+		return xInt == yInt;
 	}
 }
