adabc82c51f9f50dff0372e84526bdc8967f7899
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2022-02-22 17:33:56 -0600

    feat: added if statement and first condition

--- src/CodingExercises/EqualityPrinter.java
+++ src/CodingExercises/EqualityPrinter.java
@@ -1,4 +1,14 @@
 package CodingExercises;
 
 public class EqualityPrinter {
+
+	public static void printEqual(int one, int two, int three) {
+
+		if (one < 0 || two < 0 || three < 0) {
+			System.out.println("Invalid Value");
+
+		} else if (){
+
+		}
+	}
 }
